12758c736159258bdf9be239748b7b6e4b30b1b0
king19-9 <atyla8113@gmail.com>
2025-08-12 21:20:10 +0330

    Fix event loop issue

--- main.py
+++ main.py
@@ -2411,15 +2411,7 @@ async def main():
     await application.run_polling()
 
 if __name__ == '__main__':
-    # اصلاح مشکل event loop
-    try:
-        loop = asyncio.get_event_loop()
-    except RuntimeError as ex:
-        if "There is no current event loop in thread" in str(ex):
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-        else:
-            raise
-    
-    # اجرای برنامه
-    loop.run_until_complete(main())
+    # راه‌حل جایگزین برای مشکل event loop
+    import nest_asyncio
+    nest_asyncio.apply()
+    asyncio.run(main())
